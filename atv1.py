0b0eec741c31ab71c83e4b57f0852ab0e40679d6
cersaryuri <cesaryurisam@gamil.com>
2022-12-04 20:52:05 -0300

    alt

---
+++ atv1.py
@@ -0,0 +1,9 @@
+contador = 0
+for i in range (1,6):
+    int1 = int(input(f"Digite um núnero {i}º:"))   
+    if (int1 % 2) == 0:
+        contador += 1
+
+print(f"O número de valores pares é: {contador}")
+
+
