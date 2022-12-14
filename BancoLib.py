a3fa52e15e80aef3baa8739150f41fadbb83e33a
cersaryuri <cesaryurisam@gamil.com>
2022-12-13 23:54:36 -0300

    cre

---
+++ BancoLib.py
@@ -0,0 +1,95 @@
+import random
+
+
+class Conta():
+    def __init__(self, numConta):
+        self.numero = numConta
+        self.saldo = 0
+
+    def deposite(self, valor):
+        valor -= valor * 0.001
+        self.saldo = self.saldo + valor
+
+    def sacar(self, valor):
+        if self.saldo >= valor:
+            self.saldo = self.saldo - valor
+            return True
+        else:
+            return False
+
+class ContaBonificada(Conta):
+    
+    def __init__(self, numConta):
+        super().__init__(numConta)
+        self.acumulador = 0
+
+    def depositar(self, valor):
+        self.acumulador += valor * 0.0001
+        super().depositar(valor)
+
+    def acumularBonus(self):
+        self.saldo += self.acumulador
+        self.acumulador = 0
+
+
+class Poupanca(Conta):
+
+    def render(self):
+        self.saldo = self.saldo + self.saldo*0.01
+
+
+class Banco():
+    def __init__(self, nome):
+        self.nome = nome
+        self.contas = []
+
+    def getNome(self):
+        return self.nome
+
+    def criarConta(self):
+        num = random.randint(0, 1000)
+        c = Conta(num)
+        self.contas.append(c)
+        return num
+
+    def criarPoupanca(self):
+        num = random.randint(0, 1000)
+        p = Poupanca(num)
+        self.contas.append(p)
+        return num
+    
+    def criarCBonificada(self):
+        num = random.randint(0, 1000)
+        b = ContaBonificada(num)
+        self.contas.append(b)
+        return num
+
+    def consultaSaldo(self, numConta):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                return conta.saldo
+        return -1
+
+    def depositar(self, numConta, valor):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                conta.deposite(valor)
+
+
+    def sacar(self, numConta, valor):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                return conta.sacar(valor)
+
+    def renderPoupanca(self, numConta):
+        for i in self.contas:
+            if i.numero == numConta and isinstance(i, Poupanca):
+                i.render()
+                return True
+    
+    def acumularContaBonificad(self, numConta):
+        for i in self.contas:
+            if i.numero == numConta and isinstance(i, ContaBonificada):
+                i.acumularBonus
+                return True
+        return False
